51bc83c45a8d309b3277abf774636013727ee931
jb76 <jorgeb1976@outlook.com>
2016-09-10 21:30:41 -0600

    Add files via upload

---
+++ Empty/Empty/Empty1.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Noemy
+ * Fecha: 10/09/2016
+ * Hora: 21:23
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Test2();
+        }
+
+        private void Test1()
+        {
+            string test1 = string.Empty;
+            string test11 = test1;
+        }
+
+        private static void Test2()
+        {
+            string test2 = "";
+            string test22 = test2;
+            Console.WriteLine();
+                Console.ReadKey();
+           
+        }
+    }
+}
+
